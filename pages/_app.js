abae3678130bd9536a0e0b3b855cbc95db0753f3
davien21 <chidiebereekennia@gmail.com>
2022-01-04 11:44:38 +0100

    --fix deployment bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "antd/dist/antd.css";
 import "tailwindcss/tailwind.css";
 
 import "../index.css";
-import { Header, Loader, SideBar, Banner } from "../components";
+import { Loader, Banner } from "../components";
 import { AppProvider, useAppContext } from "../contexts/appContext";
-import { generateHand } from "../services/pokerService";
 
 function MyApp({ Component, pageProps }) {
   const [openSideBar, setOpenSideBar] = useState(false);
@@ -18,8 +16,6 @@ function MyApp({ Component, pageProps }) {
       <AppProvider>
         <Loader />
         <Banner />
-        {/* <Header onSetOpenSideBar={setOpenSideBar} /> */}
-        {/* <SideBar isOpen={openSideBar} onSetOpenSideBar={setOpenSideBar} /> */}
         <Component {...pageProps} />
       </AppProvider>
     </>
